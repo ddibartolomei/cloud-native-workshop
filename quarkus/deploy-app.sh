260a50147586534c58325c0a5acf58676b3e93a5
Daniele Di Bartolomei <daniele.dibartolomei@redhat.com>
2019-12-04 19:28:52 +0100

    Add support for native building

--- quarkus/deploy-app.sh
+++ quarkus/deploy-app.sh
@@ -3,23 +3,41 @@
 if [[ $# -eq 0 ]]; then
     SCRIPT_NAME=`basename "$0"`
     echo "Usage:"
-    echo "./$SCRIPT_NAME <project-name>"
+    echo "./$SCRIPT_NAME <project-name> <build-type>"
+    echo "  <build-type> can be one of 'jvm' or 'native'"
     exit 0
 fi
 
-if [[ $# -ne 1 ]]; then
+if [[ $# -ne 2 ]]; then
     echo "Illegal number of parameters"
     exit 1
 fi
 
 PROJECT_NAME=$1
+BUILD_TYPE=$2
+
+DOCKERFILE_TYPE=""
+COMPILE_EXTRA_PARAMS=""
+
+if [[ $BUILD_TYPE == "native" ]]; then
+    DOCKERFILE_TYPE="native"
+    COMPILE_EXTRA_PARAMS="-Pnative -Dquarkus.native.container-runtime=docker"
+    echo "------------------------------\nExecuting native build\n------------------------------\n"
+elif [[ $BUILD_TYPE == "jvm" ]]; then
+    DOCKERFILE_TYPE="jvm"
+    COMPILE_EXTRA_PARAMS=""
+    echo "------------------------------\nExecuting jvm build\n------------------------------\n"
+else
+    echo "Invalid value for parameter <build-type>: $BUILD_TYPE"
+    exit 2
+fi
 
 # compile package locally
-./mvnw clean package -DskipTests
+./mvnw clean package -DskipTests $COMPILE_EXTRA_PARAMS
 
 # Build an image on Openshift using the Docker strategy
 oc new-build --name=store-catalog --binary -l app=store-catalog -n $PROJECT_NAME
-oc patch bc/store-catalog -p "{\"spec\":{\"strategy\":{\"dockerStrategy\":{\"dockerfilePath\":\"src/main/docker/Dockerfile.jvm\"}}}}" -n $PROJECT_NAME
+oc patch bc/store-catalog -p "{\"spec\":{\"strategy\":{\"dockerStrategy\":{\"dockerfilePath\":\"src/main/docker/Dockerfile.$DOCKERFILE_TYPE\"}}}}" -n $PROJECT_NAME
 oc start-build store-catalog --from-dir=. --follow -n $PROJECT_NAME
 
 # Create an app deployment using the built image
